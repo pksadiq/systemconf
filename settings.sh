192d794debc77b9e119651985451badc24d8308a
Mohammed Sadik <sadiq@sadiqpk.org>
2016-01-09 08:07:06 +0530

    settings.sh: Trivial, rearrange settings

--- settings.sh
+++ settings.sh
@@ -1,24 +1,28 @@
 
 
+NAME="Mohammed Sadik"
+EMAIL="sadiq@sadiqpk.org"
+
 # Replace with your data
 OVERWRITE="true" # Do you wish to overwrite your current config files?
 
 EDITOR="emacs"
-NAME="Mohammed Sadik"
-EMAIL="sadiq@sadiqpk.org"
-# Scroll as on touch screen, any value other than "true" or "false"
-# can be used to leave the current settings as such
-NATURAL_SCROLL="false"
-
-# Icons, possible values: "Adwaita"
-GTK_THEME="Adwaita"
-PREFER_DARK="true"
-PREFER_DARK_TERM="true" # Prefer dark theme for Terminal Emulator?
-# Possible values: "Adwaita"
-ICON_THEME="Adwaita"
 
 # Caps Lock acts as Control Key. Any value other tahn "true" or "false"
 # can be used to leave the current settings as such.
 # Very helpful if you are an Emacs user.
 CAPS_IS_CTRL="true"
 
+# Scroll as on touch screen, any value other than "true" or "false"
+# can be used to leave the current settings as such
+NATURAL_SCROLL="false"
+
+##### Theme settings #####
+# Icons, possible values: "Adwaita"
+GTK_THEME="Adwaita"
+PREFER_DARK="true"
+PREFER_DARK_TERM="true" # Prefer dark theme for Terminal Emulator?
+
+# The main icons used
+# Possible values: "Adwaita"
+ICON_THEME="Adwaita"
