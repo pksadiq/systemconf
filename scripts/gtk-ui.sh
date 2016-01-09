6297c762ac14817039ee14d026718eb08305aa6e
Mohammed Sadik <sadiq@sadiqpk.org>
2016-01-09 08:58:55 +0530

    gtk-ui: Configure dark theme for Terminal

--- scripts/gtk-ui.sh
+++ scripts/gtk-ui.sh
@@ -3,3 +3,7 @@
 # Elements like theme and fonts.
 
 $gs ${settings}.interface gtk-theme "${GTK_THEME}"
+case "${PREFER_DARK_TERM,,}" in
+    true | false)
+	$gs org.gnome.Terminal.Legacy.Settings dark-theme "${PREFER_DARK_TERM,,}"
+esac
