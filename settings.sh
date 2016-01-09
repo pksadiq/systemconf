af71334e6cd868fab02d2cb8f25559f5f5555efb
Mohammed Sadik <sadiq@sadiqpk.org>
2016-01-09 07:58:51 +0530

    settings.sh: Move user's configuration to new file
    The user can modify this file to set the variables.
    They don't need to mess the main configure script

---
+++ settings.sh
@@ -0,0 +1,24 @@
+
+
+# Replace with your data
+OVERWRITE="true" # Do you wish to overwrite your current config files?
+
+EDITOR="emacs"
+NAME="Mohammed Sadik"
+EMAIL="sadiq@sadiqpk.org"
+# Scroll as on touch screen, any value other than "true" or "false"
+# can be used to leave the current settings as such
+NATURAL_SCROLL="false"
+
+# Icons, possible values: "Adwaita"
+GTK_THEME="Adwaita"
+PREFER_DARK="true"
+PREFER_DARK_TERM="true" # Prefer dark theme for Terminal Emulator?
+# Possible values: "Adwaita"
+ICON_THEME="Adwaita"
+
+# Caps Lock acts as Control Key. Any value other tahn "true" or "false"
+# can be used to leave the current settings as such.
+# Very helpful if you are an Emacs user.
+CAPS_IS_CTRL="true"
+
