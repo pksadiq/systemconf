160c8377df959ccf40c4e46344843691bd6a5292
Mohammed Sadik <sadiq@sadiqpk.org>
2016-01-09 09:45:05 +0530

    gtk-ui: use is_true_false

--- scripts/gtk-ui.sh
+++ scripts/gtk-ui.sh
@@ -3,7 +3,8 @@
 # Elements like theme and fonts.
 
 $gs ${settings}.interface gtk-theme "${GTK_THEME}"
-case "${PREFER_DARK_TERM,,}" in
-    true | false)
-	$gs org.gnome.Terminal.Legacy.Settings dark-theme "${PREFER_DARK_TERM,,}"
-esac
+
+if is_true_false "${PREFER_DARK_TERM}"
+then
+    $gs org.gnome.Terminal.Legacy.Settings dark-theme "${PREFER_DARK_TERM,,}"
+fi
