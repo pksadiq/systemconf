4beb6e9abf679ee7a98dffd10ec4957e28b1df6e
Mohammed Sadik <sadiq@sadiqpk.org>
2016-01-09 09:45:36 +0530

    gsettings: changing case of variable is redundant

--- scripts/gsettings.sh
+++ scripts/gsettings.sh
@@ -28,7 +28,7 @@ esac
 $gs ${settings}.peripherals.touchpad tap-to-click "true"
 $gs ${settings}.peripherals.touchpad scroll-method "two-finger-scrolling"
 
-if is_true_false "${NATURAL_SCROLL,,}"
+if is_true_false "${NATURAL_SCROLL}"
 then
    $gs ${settings}.peripherals.touchpad natural-scroll "${NATURAL_SCROLL,,}"
    echo "Natural Scrolling for touchpad state: ${NATURAL_SCROLL,,}"
