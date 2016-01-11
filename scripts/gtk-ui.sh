db6a291097ab644e65feec4c3538ba1baaab9d81
Mohammed Sadik <sadiq@sadiqpk.org>
2016-01-11 12:49:09 +0530

    gtk-ui: Set Gedit font

--- scripts/gtk-ui.sh
+++ scripts/gtk-ui.sh
@@ -44,9 +44,19 @@ then
     fi
 fi
 
+# Set Gedit font
 if is_font_present "${MONOSPACE_FONT}"
 then
     if is_number "${MONOSPACE_FONT_SIZE}"
+    then
+	$gs org.gnome.gedit.preferences.editor editor-font \
+	    "${EMACS_FONT} ${EMACS_FONT_SIZE}"
+    fi
+fi
+
+if is_font_present "${EMACS_FONT}"
+then
+    if is_number "${EMACS_FONT_SIZE}"
     then
 	$gs ${settings}.interface monospace-font-name \
 	    "${MONOSPACE_FONT_FONT} ${MONOSPACE_FONT_SIZE}"
