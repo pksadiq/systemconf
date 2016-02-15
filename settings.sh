7db079c6405e138fb6a1cf32e786b78164a8daa9
Mohammed Sadik <sadiq@sadiqpk.org>
2016-02-15 23:17:04 +0530

    settings.sh: Add TODO

--- settings.sh
+++ settings.sh
@@ -61,6 +61,7 @@ AUTO_SOFTWARE_UPDATE="false"
 # URL and IP for ping test.
 # Using IPs like Google's one shall reveal Google that
 # you are connected to internet.
+# TODO: Use this settings
 PING_IP="8.8.8.8"
 PING_URL="www.google.com"
 
