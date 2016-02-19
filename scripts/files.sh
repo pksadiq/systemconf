fd250bd30cf6c6b7b06cf370ea5eb762c11a925a
Mohammed Sadik <sadiq@sadiqpk.org>
2016-02-19 11:12:24 +0530

    scripts/files.sh: New file, Add 'Moka' theme details

---
+++ scripts/files.sh
@@ -0,0 +1,27 @@
+# Copyright (c) 2016 Mohammed Sadik <sadiq@sadiqpk.org>
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+### Icon themes ###
+# Moka Icon theme
+declare -A MOKA_ICONS
+# Test url until debugging is complete
+MOKA_ICONS[url]="http://localhost/and/moka.tar.gz"
+MOKA_ICONS[type]="tar.gz"
+MOKA_ICONS[size]="18 MiB"
+MOKA_ICONS[sha1]="0febfbc18bf24f5115a3cdceff0a5f824fe300ae"
+MOKA_ICONS[name]="Moka"
+MOKA_ICONS[dir]="moka-icon-theme-5.0/Moka"
+
+
