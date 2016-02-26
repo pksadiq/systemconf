b6d8e65bb57d4abccf78f1b4c002e0b0c29e8df7
Mohammed Sadik <sadiq@sadiqpk.org>
2016-02-26 16:07:53 +0530

    scripts/setconfs.sh: Use new function to set confs

--- scripts/setconfs.sh
+++ scripts/setconfs.sh
@@ -13,35 +13,32 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Don't configure if overwriting is disabled
-[ "${OVERWRITE,,}" != "true" ] && return 0
-
 ### GNU Bash Configuration ###
 cd "${HOME_DIR}/.bash"
-sed -i "s/^export EDITOR=.*/export EDITOR=${EDITOR}/" bashrc
+set_file "s/^export EDITOR=.*/export EDITOR=${EDITOR}/" bashrc
 # Set VISUAL editor to EDITOR, if emacs. Else use gedit
 [ ${EDITOR} = "emacs" ] && VISUAL="emacs" || VISUAL="gedit"
-sed -i "s/^export VISUAL=.*/export VISUAL=${VISUAL}/" bashrc
-sed -i "s/^export EMAIL=.*/export EMAIL=\"${EMAIL}\"/" bashrc
-is_battery_present || sed -i "s/set_battery_ps1//" bashrc
+set_file "s/^export VISUAL=.*/export VISUAL=${VISUAL}/" bashrc
+set_file "s/^export EMAIL=.*/export EMAIL=\"${EMAIL}\"/" bashrc
+is_battery_present || set_file "s/set_battery_ps1//" bashrc
 make_cd aliases
-sed -i "s/^PING_IP=.*/export PING_IP=\"${PING_IP}\"/" functions
-sed -i "s/^PING_URL=.*/export PING_URL=\"${PING_URL}\"/" functions
+set_file "s/^PING_IP=.*/export PING_IP=\"${PING_IP}\"/" functions
+set_file "s/^PING_URL=.*/export PING_URL=\"${PING_URL}\"/" functions
 cd "${HOME_DIR}"
-is_battery_present || sed -i '/ *get_battery_per/d' .bashrc
+is_battery_present || set_file '/ *get_battery_per/d' .bashrc
 
 ### gitconfig Configuration ###
-sed -i "s/^[[:blank:]]*name *=.*/    name = $NAME/" .gitconfig
-sed -i "s/^[[:blank:]]*email *=.*/    email = $EMAIL/" .gitconfig
-sed -i "s/^[[:blank:]]*editor *=.*/    editor = $EDITOR/" .gitconfig
+set_file "s/^[[:blank:]]*name *=.*/    name = $NAME/" .gitconfig
+set_file "s/^[[:blank:]]*email *=.*/    email = $EMAIL/" .gitconfig
+set_file "s/^[[:blank:]]*editor *=.*/    editor = $EDITOR/" .gitconfig
 
 ### .emacs Configuration ###
-sed -i "s/user-full-name *\".*\"/user-full-name \"${NAME}\"/" .emacs
-sed -i "s/user-mail-address *\".*\"/user-mail-address \"${EMAIL}\"/" .emacs
-sed -i "s/set-default-font *\".*\"/set-default-font \"${EMACS_FONT%* Medium}-${EMACS_FONT_SIZE}\"/" .emacs
+set_file "s/user-full-name *\".*\"/user-full-name \"${NAME}\"/" .emacs
+set_file "s/user-mail-address *\".*\"/user-mail-address \"${EMAIL}\"/" .emacs
+set_file "s/set-default-font *\".*\"/set-default-font \"${EMACS_FONT%* Medium}-${EMACS_FONT_SIZE}\"/" .emacs
 
 ### Gtk3/GNOME Configuration ###
 GTK3_CONF="${HOME_DIR}/.config/gtk-3.0/settings.ini"
 IS_DARK=$(get_true_false "$PREFER_DARK")
 [ "${IS_DARK}" ] &&
-  sed -i "s/^gtk.*prefer-dark-theme.*/gtk-application-prefer-dark-theme=${IS_DARK}/" "${GTK3_CONF}"
+  set_file "s/^gtk.*prefer-dark-theme.*/gtk-application-prefer-dark-theme=${IS_DARK}/" "${GTK3_CONF}"
