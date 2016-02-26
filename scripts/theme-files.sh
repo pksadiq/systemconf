7064f276ef11ab1d401f0bf724b4aed7e438063c
Mohammed Sadik <sadiq@sadiqpk.org>
2016-02-26 22:06:04 +0530

    theme-files.sh: Add gtk themes

---
+++ scripts/theme-files.sh
@@ -0,0 +1,14 @@
+# Copyright (c) 2016 Mohammed Sadik <sadiq@sadiqpk.org>
+
+# Gtk Theme listing
+declare -A THEMES
+THEMES["Paper"]=PAPER
+
+# Paper Gtk theme
+declare -A PAPER_THEMES
+PAPER_THEMES[url]="https://github.com/snwh/paper-gtk-theme/archive/617a6b1549d9512da2a01585e18f2ac09a4c530a.tar.gz"
+PAPER_THEMES[type]="tar.gz"
+PAPER_THEMES[size]="444 KiB"
+PAPER_THEMES[sha1]="3dcf3f0dd5b359ccde98747b7c6635e6dc074794"
+PAPER_THEMES[name]="paper-gtk-theme"
+PAPER_THEMES[dir1]="paper-gtk-theme-617a6b1549d9512da2a01585e18f2ac09a4c530a/Paper"
