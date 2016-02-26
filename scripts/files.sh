16082df01e807d46ca8a8354bcc2c5a370ba34d1
Mohammed Sadik <sadiq@sadiqpk.org>
2016-02-26 22:01:58 +0530

    scripts/files.sh: Add Paper gtk theme

--- scripts/files.sh
+++ scripts/files.sh
@@ -22,6 +22,19 @@ ICONS["Numix-Light"]=NUMIX
 ICONS["Numix-Circle"]=NUMIX_CIRCLE
 ICONS["Numix-Circle-Light"]=NUMIX_CIRCLE
 
+# Gtk Theme listing
+declare -A THEMES
+THEMES["Paper"]=PAPER
+
+# Paper Gtk theme
+declare -A PAPER_THEMES
+PAPER_THEMES[url]="https://github.com/snwh/paper-gtk-theme/archive/617a6b1549d9512da2a01585e18f2ac09a4c530a.tar.gz"
+PAPER_THEMES[type]="tar.gz"
+PAPER_THEMES[size]="444 KiB"
+PAPER_THEMES[sha1]="3dcf3f0dd5b359ccde98747b7c6635e6dc074794"
+PAPER_THEMES[name]="paper-gtk-theme"
+PAPER_THEMES[dir1]="paper-gtk-theme-617a6b1549d9512da2a01585e18f2ac09a4c530a/Paper"
+
 ### Icon themes ###
 # Moka Icon theme
 declare -A MOKA_ICONS
